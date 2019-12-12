ecf50da627e047c54bbd8424a1b33f43f78191e9
Student <example@example.com>
2019-12-12 13:45:28 -0800

    add cool ascii art.

--- lib/primes.rb
+++ lib/primes.rb
@@ -1,14 +1,7 @@
 class Primes
-
   def sieve(max)
     nums = (2..max).to_a
-    nums.map do |e|
-      nums.each do |x|
-        if ((x % e) == 0) & (x != e)
-          nums.delete(x)
-        end
-      end
-    end
+    nums.each { |e| nums.reject!{ |x| ((x % e) == 0) & (x != e)} }
     return nums
   end
 end
